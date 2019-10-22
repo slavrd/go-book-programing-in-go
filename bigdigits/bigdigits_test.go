076c9a577bc1af7f9a7871fde018b19513481e06
Slav Donchev <slav@Slavs-MacBook-Pro.local>
2019-10-22 10:57:45 +0300

    add print with bar tests

--- bigdigits/bigdigits_test.go
+++ bigdigits/bigdigits_test.go
@@ -1,10 +1,10 @@
 // Copyright © 2011-12 Qtrac Ltd.
-// 
+//
 // This program or package and any associated files are licensed under the
 // Apache License, Version 2.0 (the "License"); you may not use these files
 // except in compliance with the License. You can get a copy of the License
 // at: http://www.apache.org/licenses/LICENSE-2.0.
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,42 +14,63 @@
 package main
 
 import (
-    "bytes"
-    "io/ioutil"
-    "log"
-    "os"
-    "os/exec"
-    "path/filepath"
-    "testing"
+	"bytes"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
 )
 
 func TestBigDigits(t *testing.T) {
-    log.SetFlags(0)
-    log.Println("TEST bigdigits")
+	log.SetFlags(0)
+	log.Println("TEST bigdigits")
 
-    path, _ := os.Getwd()
-    expected, err := ioutil.ReadFile(filepath.Join(path, "0123456789.txt"))
-    if err != nil {
-        t.Fatal(err)
-    }
-    executable := filepath.Join(path, "bigdigits")
-    reader, writer, err := os.Pipe()
-    if err != nil {
-        t.Fatal(err)
-    }
-    command := exec.Command(executable, "0123456789")
-    command.Stdout = writer
-    err = command.Run()
-    if err != nil {
-        t.Fatal(err)
-    }
-    writer.Close()
-    actual, err := ioutil.ReadAll(reader)
-    if err != nil {
-        t.Fatal(err)
-    }
-    reader.Close()
-    if bytes.Compare(actual, expected) != 0 {
-        t.Fatal("actual != expected")
-    }
+	path, _ := os.Getwd()
+	executable := filepath.Join(path, "bigdigits")
+
+	type testCase struct {
+		command     *exec.Cmd
+		expFileName string
+	}
+
+	tests := []testCase{
+		{
+			command:     exec.Command(executable, "0123456789"),
+			expFileName: "0123456789.txt",
+		},
+		{
+			command:     exec.Command(executable, "--bar", "0123456789"),
+			expFileName: "0123456789-bar.txt",
+		},
+	}
+
+	for _, test := range tests {
+
+		expected, err := ioutil.ReadFile(filepath.Join(path, test.expFileName))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		reader, writer, err := os.Pipe()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		test.command.Stdout = writer
+		err = test.command.Run()
+		if err != nil {
+			t.Fatal(err)
+		}
+		writer.Close()
+		actual, err := ioutil.ReadAll(reader)
+		if err != nil {
+			t.Fatal(err)
+		}
+		reader.Close()
+		if bytes.Compare(actual, expected) != 0 {
+			t.Fatal("actual != expected")
+		}
+	}
 }
